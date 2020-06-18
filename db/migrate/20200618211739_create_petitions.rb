071225e686a177089511213077fb2cbc52bff026
d-otis <dan@dan-foley.com>
2020-06-18 17:35:27 -0400

    add author_id to petitions migration

--- db/migrate/20200618211739_create_petitions.rb
+++ db/migrate/20200618211739_create_petitions.rb
@@ -1,6 +1,7 @@
 class CreatePetitions < ActiveRecord::Migration[6.0]
   def change
     create_table :petitions do |t|
+      t.integer :author_id
       t.string :title
       t.text :description
       t.integer :goal
