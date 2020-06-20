b27579c21bcbeb95f6c8a7ddbe3696298345f5ee
d-otis <dan@dan-foley.com>
2020-06-20 16:39:30 -0400

    add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/signin' => "sessions#new"
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :petitions
