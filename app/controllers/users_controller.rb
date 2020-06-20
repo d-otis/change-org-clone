f0709140016af44a8e8728e35a1181301c73056c
d-otis <dan@dan-foley.com>
2020-06-20 15:40:27 -0400

    add user controller and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+    binding.pry
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
