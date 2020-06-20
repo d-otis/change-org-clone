28bae76d1b6bd996287a5ca959c4f8977244c576
d-otis <dan@dan-foley.com>
2020-06-20 17:02:42 -0400

    define controller actions for new/create petitions with their corresponding forms and helper methods

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -1,4 +1,8 @@
 class PetitionsController < ApplicationController
+	before_action :set_petition, only: [:show, :edit, :update, :destroy]
+	before_action :require_login
+	skip_before_action :require_login, only: [:index, :show]
+
 	def index
 	end
 
@@ -12,9 +16,17 @@ class PetitionsController < ApplicationController
 	end
 
 	def new
+		@petition = Petition.new
 	end
 
 	def create
+		@petition = Petition.new(petition_params)
+		
+		if @petition.save
+			redirect_to petition_path(@petition), notice: "You have created a new petition!"
+		else
+			render :new
+		end
 	end
 
 	def destroy
@@ -23,5 +35,14 @@ class PetitionsController < ApplicationController
 	private
 
 	def petition_params
+		params.require(:petition).permit(:title, :description, :goal, :author_id)
+	end
+
+	def require_login
+		redirect_to "/signin", notice: "You must be signed in to do that." unless session[:user_id]
+	end
+
+	def set_petition
+		@petition = Petition.find_by(id: params[:id])
 	end
 end
