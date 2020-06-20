8c4bb2d7d2a5752ffbbee1a84c8c1f1ee1fd3341
d-otis <dan@dan-foley.com>
2020-06-20 12:39:05 -0400

    remove erroneously named elements

--- app/models/petition_signer.rb
+++
@@ -1,2 +0,0 @@
-class PetitionSigner < ApplicationRecord
-end
--- test/models/petition_signer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PetitionSignerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
