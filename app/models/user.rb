08ceea2e2e27ae2e1cdbc902338413d91499fd50
d-otis <dan@dan-foley.com>
2020-06-20 14:23:24 -0400

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
 			 :class_name => "Petition", 
 			 :foreign_key => :author_id
 	has_many :signatures
+	has_secure_password
+	validates :name, :presence => true
+	validates :email, :presence => true, :uniqueness => true
+	validates :password, :presence => true, :confirmation => true
+	validates_confirmation_of :password
+	validates :password_confirmation, :presence => true
 end
