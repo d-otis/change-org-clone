fc85de25cc313311ec8b1f502c0ef50e72a09735
d-otis <dan@dan-foley.com>
2020-06-21 19:53:18 -0400

    add logic for displaying signature based off user consent

--- app/helpers/petitions_helper.rb
+++ app/helpers/petitions_helper.rb
@@ -2,4 +2,8 @@ module PetitionsHelper
 	def display_date(date_time)
 		date_time.strftime("%b %e, %l:%M %p")
 	end
+
+	def display_signer_name(signature)
+		signature.anonymous ? "Anonymous" : signature.user_name
+	end
 end
