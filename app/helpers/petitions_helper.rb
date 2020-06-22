f0795570d886271aeb0e2536a6df1584828ca1d6
d-otis <dan@dan-foley.com>
2020-06-22 19:31:59 -0400

    update petitions helper

--- app/helpers/petitions_helper.rb
+++ app/helpers/petitions_helper.rb
@@ -8,6 +8,18 @@ module PetitionsHelper
 	end
 
 	def display_message(signature)
-		"#{signature.message.strip}" if signature.message.present?
+		"#{signature.message.strip.capitalize}" if signature.message.present?
+	end
+
+	def has_signed?(petition)
+		if Signature.where("user_id = ? and petition_id = ?", current_user.id, petition.id).count >= 1
+			true
+		else
+			false
+		end
+	end
+
+	def is_author?(petition)
+		petition.author == current_user
 	end
 end
