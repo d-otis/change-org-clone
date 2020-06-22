dfe82bef98a6e8a4ab7c4f1e624841f0017b09df
d-otis <dan@dan-foley.com>
2020-06-22 18:16:30 -0400

    fix sessions redirect error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   		session[:user_id] = @user.id
   		redirect_to user_path(@user)
   	else
-  		redirect_to '/signin', notice: @user.errors
+  		redirect_to '/signin', notice: "Incorrect credentials."
   	end
   end
 
