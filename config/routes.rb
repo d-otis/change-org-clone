2449c8146cf2e00a8bcf5cf9c09572ebc6213aef
d-otis <dan@dan-foley.com>
2020-06-23 14:30:37 -0400

    add admin namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,16 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :petitions
-  resources :users
-  resources :signatures
+  resources :petitions, :users, :signatures
+
+  scope '/dashboard' do
+  	# get '/', to: 'users#dashboard'
+  	resources :users, only: [:show, :edit, :update, :destroy]
+  	resources :petitions
+  	resources :signatures, only: [:edit, :destroy]
+  end
+
+  get '/dashboard', to: 'users#dashboard'
 
   root 'sessions#new'
 
