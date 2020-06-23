3c58c11d308a9599fe6b48bd7aabdffbc76555a9
d-otis <dan@dan-foley.com>
2020-06-23 16:48:50 -0400

    add functionality to delete your signature

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,4 +1,5 @@
 class SignaturesController < ApplicationController
+	before_action :require_ownership, only: [:destroy]
 
 	def create
 		Signature.create(signature_params)
@@ -6,9 +7,20 @@ class SignaturesController < ApplicationController
 		redirect_to petition_path(signature_params[:petition_id])
 	end
 
+	def destroy
+		@petition = @signature.petition
+		@signature.destroy
+		redirect_to petition_path(@petition), notice: "You signature has been removed."
+	end
+
 	private
 
 	def signature_params
 		params.require(:signature).permit(:anonymous, :message, :user_id, :petition_id)
+	end
+
+	def require_ownership
+		@signature = Signature.find(params[:id])
+		redirect_to dashboard_path unless current_user == @signature.user
 	end
 end
