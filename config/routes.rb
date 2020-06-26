6ec1202cfa9955c3e5bfe3ec602aa79e718dccc9
d-otis <dan@dan-foley.com>
2020-06-26 18:05:55 -0400

    add facebook auth callback routing to sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/signin' => "sessions#new"
   post '/sessions' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+  get '/auth/facebook/callback' => 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :petitions do
