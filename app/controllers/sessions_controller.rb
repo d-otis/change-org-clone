12ca0e5264d66dccf8c19f8caee352b260ed05fb
d-otis <dan@dan-foley.com>
2020-06-26 18:02:11 -0400

    incorporate omniauth into sessions#create and and user creation within that session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	@user = User.find_by(email: user_params[:email])
-  	if @user && @user.authenticate(user_params[:password])
-  		session[:user_id] = @user.id
-  		redirect_to user_path(@user)
-  	else
-  		redirect_to '/signin', notice: "Incorrect credentials."
-  	end
+    if auth
+      @user = User.find_or_create_by(email: auth['info']['email']) do |u|
+        u.password = generate_password
+        u.name = auth['info']['name']
+      end
+      session[:user_id] = @user.id
+      redirect_to user_path(@user) and return
+    else
+      @user = User.find_by(email: user_params[:email])
+      if @user && @user.authenticate(user_params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user) and return
+      else
+        redirect_to '/signin', notice: "Incorrect credentials."
+      end
+    end
+
   end
 
   def destroy
@@ -22,5 +32,14 @@ class SessionsController < ApplicationController
 
   def user_params
   	params.require(:user).permit(:email, :password)
+  end
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
+  def generate_password
+    salt = BCrypt::Engine::generate_salt
+    hash = BCrypt::Engine::hash_secret(salt, salt)
   end
 end
