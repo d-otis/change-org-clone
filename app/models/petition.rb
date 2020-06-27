35014c4ec3c60e07baf2030427f4d3846d11869e
d-otis <dan@dan-foley.com>
2020-06-27 18:03:31 -0400

    add AR scope method to Petition model to return petition with the most signatures

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -26,6 +26,20 @@ class Petition < ApplicationRecord
 		self.signatures.order(:created_at => :desc).first(5)
 	end
 
+	def self.most_signatures
+		joins(:signatures).group("petitions.id").order(Arel.sql("COUNT(*) DESC"))
+	end
+
+	def self.signature_report
+		joins(:signatures).group("petitions.id").order(Arel.sql("COUNT(*) DESC")).count
+		# returns a hash the following where key is petition id and value is number of signatures 
+		# ordered DESC by number for signatures
+		# {
+		# 	3=>7, 4=>6, 2=>6, 11=>5, 8=>5, 5=>5, 16=>4, 14=>4, 7=>4, 19=>3, 
+		# 	17=>3, 15=>3, 12=>3, 10=>2, 9=>2, 6=>2, 1=>2, 22=>1, 21=>1, 20=>1, 18=>1, 13=>1
+		# }
+	end
+
 	def signature_need
 		self.goal - self.signature_count
 	end
