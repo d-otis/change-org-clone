29ef2e39a3b7518361656a5d836291f92c606cf4
d-otis <dan@dan-foley.com>
2020-06-29 20:17:13 -0400

    fix bug where app crashed trying to display signature of user who does't exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       render :new
--- app/helpers/signatures_helper.rb
+++ app/helpers/signatures_helper.rb
@@ -1,2 +1,15 @@
 module SignaturesHelper
+	def display_sig_usr(signature)
+		signature.anonymous ? "Anonymous" : signature.user_name
+	end
+
+	def sig_usr_link(signature)
+		if signature.anonymous
+			#  don't show link
+			content_tag(:span, "Anonymous",:class => 'badge badge-pill badge-danger')
+		else
+			# show link
+			link_to signature.user_name, user_path(signature.user), :class => 'badge badge-pill badge-danger'
+		end
+	end
 end
