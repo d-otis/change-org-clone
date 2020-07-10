8e44239e5a03abecc00b9b90dc430279754764e0
d-otis <dan@dan-foley.com>
2020-07-10 16:56:32 -0400

    add User model tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+	context "validations" do
+		let(:valid_attributes) do {
+			name: "Trent Reznor",
+			email: "treznor@gmail.com",
+			password: "1234",
+			password_confirmation: "1234"
+		}
+		end
+
+		let(:missing_name) { valid_attributes.except(:name) }
+		let(:missing_email) { valid_attributes.except(:email) }
+		let(:missing_password) { valid_attributes.except(:password) }
+		let(:missing_password_confirmation) { valid_attributes.except(:password_confirmation) }
+		let(:password_mismatch) { valid_attributes.merge( { :password_confirmation => '0000' } ) }
+
+		it 'has a password field' do
+			expect(User.new).to respond_to(:password)
+		end
+
+		it "has a password confirmation field" do
+			expect(User.new).to respond_to(:password_confirmation)
+		end
+
+		it "has a name field" do
+			expect(User.new).to respond_to(:name)
+		end
+
+		it "has an email field" do
+			expect(User.new).to respond_to(:email)
+		end
+
+		it "is valid with name, password, confirmation, and email" do
+			expect(User.new(valid_attributes)).to be_valid
+		end
+
+		it "is invalid without a name" do
+			expect(User.new(missing_name)).to be_invalid
+		end
+
+		it "is invalid without an email" do
+			expect(User.new(missing_email)).to be_invalid
+		end
+
+		it "created_with_oauth defaults to false" do
+			expect(User.new(valid_attributes).created_with_oauth).to be false
+		end
+
+		it "is invalid when password and confirmation do not match" do
+			expect(User.new(password_mismatch)).to be_invalid
+		end
+	end
+end
