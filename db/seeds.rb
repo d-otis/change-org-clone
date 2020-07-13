9f5e5fcd42255158865ab536d45e52ef86930913
d-otis <dan@dan-foley.com>
2020-07-13 16:57:03 -0400

    assign categorie sin seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,75 +8,82 @@
 
 
 def mega_seed(users, signatures)
-	puts ""
-	puts "Beginning Seed..."
-	puts ""
+  puts ""
+  puts "Beginning Seed..."
+  puts ""
 
-	make_users(users)
-	make_petitions
-	make_signatures(signatures)
+  make_users(users)
+  make_petitions
+  make_signatures(signatures)
 end
 
 
 def make_users(num)
-	num.times do
-		u = User.create(
-			:name => Faker::Name.unique.name,
-			:email => Faker::Internet.unique.email,
-			:password => pwd,
-			:password_confirmation => pwd)
-		puts "#{u.id}. #{u.name} has joined"
-	end
+  num.times do
+    u = User.create(
+      :name => Faker::Name.unique.name,
+      :email => Faker::Internet.unique.email,
+      :password => pwd,
+      :password_confirmation => pwd)
+    puts "#{u.id}. #{u.name} has joined"
+  end
 
-	puts ""
-	puts "Created #{num} users..."
-	puts ""
+  puts ""
+  puts "Created #{num} users..."
+  puts ""
 end
 
 def make_petitions
-	randomized_users = (1..User.count).sort{ rand() - 0.5 }
+  randomized_users = (1..User.count).sort{ rand() - 0.5 }
 
-	randomized_users.each do |p|
-		rand(0..3).times do
-			Petition.create(
-				:title => Faker::Lorem.sentence(word_count: rand(5..11)), 
-				:description => LoremIpsumText::multiple_para(rand(10..30)).join("\r\n\r\n"),
-				:author_id => p, 
-				:goal => rand(12..20))
-		end
-	end
+  randomized_users.each do |p|
+    rand(0..3).times do
+      Petition.create(
+        :title => Faker::Lorem.sentence(word_count: rand(5..11)), 
+        :description => LoremIpsumText::multiple_para(rand(10..30)).join("\r\n\r\n"),
+        :author_id => p, 
+        :goal => rand(12..20))
+    end
+  end
 
-	puts ""
-	puts "Created #{Petition.count} petitions..."
-	puts ""
+  puts ""
+  puts "Created #{Petition.count} petitions..."
+  puts ""
 end
 
 
 
 def make_signatures(num)
 
-	num.times do
-		petition = Petition.find(rand(1..Petition.count))
-		# Need to randomly select a user and only make signature join if their user object is already in the join
-		user = User.find(rand(1..User.count))
-		# query DB for Signature where user_id and petition_id
-		if Signature.where(petition: petition, user: user).empty?
-			petition.signatures.create(
-				:user => user, 
-				:message => Faker::Lorem.sentences(number: rand(3..10)).join(" "), 
-				:anonymous => rand(0..1))
-		end
-	end
+  num.times do
+    petition = Petition.find(rand(1..Petition.count))
+    # Need to randomly select a user and only make signature join if their user object is already in the join
+    user = User.find(rand(1..User.count))
+    # query DB for Signature where user_id and petition_id
+    if Signature.where(petition: petition, user: user).empty?
+      petition.signatures.create(
+        :user => user, 
+        :message => Faker::Lorem.sentences(number: rand(3..10)).join(" "), 
+        :anonymous => rand(0..1))
+    end
+  end
 
-	puts ""
-	puts "Created #{Signature.count} signatures..."
-	puts ""
+  puts ""
+  puts "Created #{Signature.count} signatures..."
+  puts ""
 end
 
 def make_categories
   categories = ["Racial Justice", "Economic Justice", "Human Rights", "Women's Rights", "Animals", "Politics"]
   categories.each do |cat|
     Category.create(:title => cat)
+  end
+end
+
+def assign_categories
+  petitions = Petition.all
+  petitions.each do |p|
+    p.update(category_id: rand(1..Category.count))
   end
 end
 
@@ -94,3 +101,4 @@ dan = User.create(name: "Dan Foley", email: "dan@dan-foley.biz", password: pwd, 
 pookie.authored_petitions.create(title: "End Scooter McBooberson’s Reign of Terror", description: "She stinks!", goal: 5)
 
 make_categories
+assign_categories
