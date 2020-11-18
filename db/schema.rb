d0cbe912865aa1bd143562293104faf47950b62e
d-otis <dan@dan-foley.com>
2020-11-18 15:44:44 -0500

    generate new db schema with PG extension activated

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_13_173016) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
