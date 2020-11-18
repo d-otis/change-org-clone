5915878e4957c85414410cadef76b660cebc9d92
d-otis <dan@dan-foley.com>
2020-11-18 17:38:00 -0500

    force ssl in application config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Change
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.time_zone = 'Eastern Time (US & Canada)'
+    config.force_ssl = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
